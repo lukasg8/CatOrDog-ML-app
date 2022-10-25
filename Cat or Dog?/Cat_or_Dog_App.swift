391ef2f68bcc38f560fedfd806405ea48b1c1ba2
lukasg8 <102938476+lukasg8@users.noreply.github.com>
2022-10-25 16:26:26 -0500

    Initial Commit

---
+++ Cat or Dog?/Cat_or_Dog_App.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_or_Dog_App.swift
+//  Cat or Dog?
+//
+//  Created by Lukas on 10/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Cat_or_Dog_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
